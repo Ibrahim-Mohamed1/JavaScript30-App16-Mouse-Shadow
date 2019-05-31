8275e4c392a486ab99fd048a37cbaee8f24e7c3d
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-30 19:26:14 -0600

    update

--- App.js
+++ App.js
@@ -5,4 +5,8 @@ const hero = document.querySelector('.hero');
   function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;
     let { offsetX: x, offsetY: y } = e;
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+      }
   }
